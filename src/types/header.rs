72728b7c0069e2775f3f0c9beca9599edac2c4cf
jonasrsv42 <jonasrsv42@gmail.com>
2026-02-11 22:22:31 +0100

    hey, what's that over there?!</p>

--- src/types/header.rs
+++ src/types/header.rs
@@ -4,7 +4,7 @@
 pub const MAGIC: &[u8; 8] = b"BLOBFIG\0";
 
 /// Current format version
-pub const VERSION: u32 = 2;
+pub const VERSION: u32 = 1;
 
 /// Header size in bytes (magic + version + flags)
 pub const HEADER_SIZE: usize = 16;
